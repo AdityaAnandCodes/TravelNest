f3f954bb3684469a54ed5d82eb2b93dc40b4ad6c
AdityaAnandCodes <adityaanandatwork276@gmail.com>
2024-11-14 17:06:22 +0530

    udate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
+
 import Home from "../src/pages/Home";
 import Destinations from "../src/pages/Destinations";
 import DestinationDetails from "../src/pages/DestinationDetails"; // Import DestinationDetails
-=======
-import Home from "./pages/Home";
-import Destinations from "./pages/Destinations";
-import DestinationDetail from "./pages/DestinationDetail"; // Import DestinationDetails
->>>>>>> 916883156bee3ec7c61c6898421d82a5f764ddef
+
 import Navbar from "../src/components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Authentication from "../src/pages/Authentication";
@@ -21,7 +17,7 @@ const App = () => {
           <Route path="/destinations" element={<Destinations />}></Route>
           <Route
             path="/destinations/:id"
-            element={<DestinationDetail />}
+            element={<DestinationDetails />}
           ></Route>
           <Route path="/authentication" element={<Authentication />}></Route>
         </Routes>
