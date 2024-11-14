fa0191d315791290f8adc7e607ed6e6eb0b1374c
raksha-bv <rakshabv04@gmail.com>
2024-11-14 14:59:58 +0530

    destinations update

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -4,18 +4,21 @@ import destinations from "../constants/index";
 
 const Destinations = () => {
   return (
-    <div
-      className="p-8 min-h-screen bg-gray-100"
-      style={{
-        backgroundImage: "url(path-to-your-image.jpg)", // Replace with your background image path
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">
-        Explore Destinations
-      </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    <div className="bg-gray-100 min-h-screen">
+      <div className="relative">
+        <img
+          src="/bg_destinations.jpg"
+          alt="Destinations Background"
+          className="min-w-full  object-cover"
+        />
+        {/* Overlay Text */}
+        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl font-bold">
+          Explore Destinations
+        </h1>
+      </div>
+
+      {/* Destination Cards */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-8 p-8">
         {destinations.map((destination, index) => (
           <DestinationCard
             key={index}
