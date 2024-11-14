e1d5c4ff0361399c3529a4432087bb090f742deb
raksha-bv <rakshabv04@gmail.com>
2024-11-14 14:50:50 +0530

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,33 @@
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
   const navigate = useNavigate();
+
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="fixed top-0 left-0 w-full z-20 flex justify-between items-center p-5 bg-transparent bg-opacity-0">
+    <nav
+      className={`fixed top-0 left-0 w-full z-20 flex justify-between items-center p-5 transition-all ${
+        scrolled ? "bg-black" : "bg-transparent bg-opacity-0"
+      }`}
+    >
       <div className="flex items-center gap-6">
         <div
           className="text-white text-3xl font-semibold cursor-pointer"
