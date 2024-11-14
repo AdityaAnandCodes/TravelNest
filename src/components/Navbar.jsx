6a786a88a8dab8ead41b40a63769ba5caf55c4bb
raksha-bv <rakshabv04@gmail.com>
2024-11-14 12:32:47 +0530

    navbar init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <section className="flex justify-between items-center bg-black min-w-full">
+      <div className="flex justify-between items-center p-5">
+        <div className="sour-gummy-logo text-lg text-white ">TravelNest</div>
+      </div>
+    </section>
+  );
+};
 
-export default Navbar
+export default Navbar;
